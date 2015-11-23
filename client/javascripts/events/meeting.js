cc31d9adc28c646d3cd1e1037b4cc9a4095071f4
chaussy <maxime.chaussy@gmail.com>
2015-11-23 16:30:15 +0100

    Ajout du drag & drop pour lordre des demandes de paroles

--- client/javascripts/events/meeting.js
+++ client/javascripts/events/meeting.js
@@ -1,5 +1,47 @@
 var timerId = 0;
 
+
+ //Add some handlebars helpers to our Template.
+  //  This one handily enough returns our Items in rank order
+  //  Since Meteor is reactive, whenever our Items change Meteor
+  //    will re-render our Template (putting them in the correct order)
+ 
+  //Once the Template is rendered, run this function which
+  //  sets up JQuery UI's sortable functionality
+  Template.meeting.rendered = function() {
+    this.$('#list-group').sortable({
+        stop: function(e, ui) {
+          // get the dragged html element and the one before
+          //   and after it
+          el = ui.speech.get(0)
+          before = ui.speech.prev().get(0)
+          after = ui.speech.next().get(0)
+ 
+          // Here is the part that blew my mind!
+          //  Blaze.getData takes as a parameter an html element
+          //    and will return the data context that was bound when
+          //    that html element was rendered!
+          if(!before) {
+            //if it was dragged into the first position grab the
+            // next element's data context and subtract one from the rank
+            newRank = Blaze.getData(after).rank - 1
+          } else if(!after) {
+            //if it was dragged into the last position grab the
+            //  previous element's data context and add one to the rank
+            newRank = Blaze.getData(before).rank + 1
+          }
+          else
+            //else take the average of the two ranks of the previous
+            // and next elements
+            newRank = (Blaze.getData(after).rank +
+                       Blaze.getData(before).rank)/2
+ 
+          //update the dragged Item's rank
+          Speeches.update({id: Blaze.getData(el).id}, {$set: {rank: newRank}})
+        }
+    })
+  }
+  
 /** The events that meeting template contains */
 Template.meeting.events({
     /** A click on talkCancel opens the lineup page or cancels the user's speech */
@@ -218,6 +260,10 @@ Template.meeting.helpers ({
 
     isAnimator: function() {
         return Users.findOne({_id: Session.get("userId")}).type == "animator";
+    },
+	
+	 sortedSpeeches: function() {
+      return Speeches.find({}, {sort: {rank: 1}})
     }
 });/*
 $(document).ready(function(){
